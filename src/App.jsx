986de048673211d5ed8c1d3471aff7cfd7bf58cb
kiianskii <ankiyanskiy@gmail.com>
2024-07-03 14:42:29 +0300

    added redux files

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import { Route, Routes } from "react-router-dom";
 import { Suspense } from "react";
 import Layout from "./components/Layout/Layout";
 import HomePage from "./pages/HomePage/HomePage";
+import TestPage from "./pages/TestPage/TestPage";
 
 function App() {
   return (
     <Suspense>
       <Routes>
-        <Route path="/" element={<Layout />} />
-        <Route index element={<HomePage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="test" element={<TestPage />} />
+        </Route>
       </Routes>
     </Suspense>
   );
