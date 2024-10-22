b7377cfaeb60ec486f74f2d74cbbbe63bde04017
kiianskii <ankiyanskiy@gmail.com>
2024-10-22 16:11:02 +0300

    Styled materials page

--- src/pages/MaterialsPage/MaterialsPage.tsx
+++ src/pages/MaterialsPage/MaterialsPage.tsx
@@ -7,19 +7,17 @@ const MaterialsPage = () => {
       <div className={s.literature}>
         <h3 className={s.title}>Useful literature</h3>
         <ol className={s.lit_list}>
-          <li className={s.lit_item}>Testing dot.com Savin.</li>
-          <li className={s.lit_item}>
-            A mental hospital in the hands of patients.
-          </li>
-          <li className={s.lit_item}>Scrum. J. Sutherland.</li>
+          <li>Testing dot.com Savin.</li>
+          <li>A mental hospital in the hands of patients.</li>
+          <li>Scrum. J. Sutherland.</li>
         </ol>
       </div>
       <div className={s.resource}>
         <h3 className={s.title}>Useful resources</h3>
         <ol className={s.lit_list}>
-          <li className={s.lit_item}>dou.ua</li>
-          <li className={s.lit_item}>Habr</li>
-          <li className={s.lit_item}>facebook.com/QA</li>
+          <li>dou.ua</li>
+          <li>Habr</li>
+          <li>facebook.com/QA</li>
         </ol>
       </div>
     </section>
