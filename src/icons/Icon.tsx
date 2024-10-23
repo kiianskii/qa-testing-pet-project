021c7b8c9e15303fb4b6ca440af3b370c5f330ff
kiianskii <ankiyanskiy@gmail.com>
2024-10-23 13:34:06 +0300

    upd

--- src/icons/Icon.tsx
+++ src/icons/Icon.tsx
@@ -13,12 +13,3 @@ export const Icon: React.FC<PropsType> = ({ id, size, className }) => {
     </svg>
   );
 };
-
-// export default Icons;
-
-//Щоб використати іконки потрібно імпортувати даний файл до себе у компонент: import { Icon } from "../../img/Icon";
-
-//Пізніше добавляємо у потрібному місті: <Icon size={17.1} id="logo-mob" />
-// В id="" прописуємо назву іконки без вставки "icon-", тільки нп. "home", "exit" і тд.
-// У size прописуємо відповідні розміри
-// У className звичайно прописуємо класи
